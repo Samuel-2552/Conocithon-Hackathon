f19856f823601eb464cfbc24bd54d696264a17bf
Shivesh16 <92038870+Shivesh16@users.noreply.github.com>
2023-03-11 01:13:21 +0530

    locationcode debugging
    debugging

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, render_template
+from geopy.geocoders import Nominatim
+import jsonify
+
 
 app = Flask(__name__)
 
@@ -6,12 +9,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/location', methods=['POST'])
+@app.route('/location', methods=['GET'])
 def location():
-    latitude = request.form['latitude']
-    longitude = request.form['longitude']
-    # do something with the location data
-    return "Received location: {}, {}".format(latitude, longitude)
+    latitude = request.args.get('lat')
+    longitude = request.args.get('long')
+    return jsonify({'latitude': latitude, 'longitude': longitude})
 
 if __name__ == '__main__':
     app.run()

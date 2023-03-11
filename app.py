3261cf14fe6bd30a50d9aa95f9ba2b1ed1641fd3
Shiva250503ss <105617004+Shiva250503ss@users.noreply.github.com>
2023-03-11 12:31:40 +0530

    verification
    added verification

--- app.py
+++ app.py
@@ -1,34 +1,61 @@
-from flask import Flask, request, render_template
-from geopy.geocoders import Nominatim
-import os
+import smtplib
+import ssl
+import sqlite3
+import math
+import random
 
+smtp_port = 587                 # Standard secure SMTP port
+smtp_server = "smtp.gmail.com"  # Google SMTP Server
+email = "201501049@rajalakshmi.edu.in"
 
-images=os.path.join('static','images')
+def OTP():
+    digits = "0123456789"
+    OTP = ""
+    for i in range(6):
+        OTP += digits[math.floor(random.random() * 10)]
+    return OTP
 
-app = Flask(__name__)
-app.config['icons'] = images
+    # global otp
+    
 
-logo = os.path.join(app.config['icons'], 'logo.png')
-fav_icon = os.path.join(app.config['icons'], 'fav_icon.png')
+    # Connect to the database
+conn_cred = sqlite3.connect('cred.db')
 
+    # Create a cursor
+cursor_cred = conn_cred.cursor()
 
+    # Execute an SQL command to retrieve the first row from the user table
+cursor_cred.execute('SELECT * FROM user LIMIT 1')
 
-@app.route('/')
-def index():
-    return render_template('index.html',logo=logo,fav_icon=fav_icon)
+    # Fetch the first row and print it
+row = cursor_cred.fetchone()
+email_from=row[1]
+decrypted=row[2]
 
-@app.route('/update-location', methods=['POST'])
-def update_location():
-    data = request.get_json()
-    geolocator = Nominatim(user_agent="my-app-name")
-    latitude = data['latitude']
-    longitude = data['longitude']
-    # Do something with the latitude and longitude values
-    print("lattitude", latitude)
-    print("Longitude", longitude)
-    location = geolocator.reverse(f"{latitude}, {longitude}", zoom=18)
-    out=location
-    return f"<br>Your Address: {out}"
+    # Close the connection
+conn_cred.close()
 
-if __name__ == '__main__':
-    app.run()
+try:
+    otp = OTP() + " is your OTP"
+    subject = f"Welcome to Printconnect !"
+    message = f"{otp}.\nPlease do not share it with anyone."
+        # Create context
+    simple_email_context = ssl.create_default_context()
+    
+# Connect to the server
+    print("Connecting to server...")
+    TIE_server = smtplib.SMTP(smtp_server, smtp_port)
+    TIE_server.starttls(context=simple_email_context)
+    TIE_server.login(email_from, decrypted)
+    print("Connected to server :-)")
+
+    # Send the actual email
+    print()
+    print(f"Sending email to - {email}")
+    TIE_server.sendmail(email_from, email, f"Subject: {subject}\n\n{message}")
+    print(f"Email successfully sent to - {email}")
+
+         # If there's an error, print it out
+
+except Exception as e:
+    print(e)
